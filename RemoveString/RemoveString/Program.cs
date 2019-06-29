cf88c1b41b4135cdaa8de68647562992e8a21b6e
Funbi oyede <samsonoyede@yahoo.com>
2019-06-29 14:59:12 +0100

    tests remove first and last characters of a string

--- RemoveString/RemoveString/Program.cs
+++ RemoveString/RemoveString/Program.cs
@@ -4,14 +4,14 @@
 
 namespace RemoveString
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+    public static void Main(string[] args)
         {
             RemoveFirstLast("Backspace");
       
         }
-        static string RemoveFirstLast(string word)
+       public static string RemoveFirstLast(string word)
         {
             string firstLetter = word.Remove(0,1);
             string SecondLetter = firstLetter.Remove(firstLetter.Length - 1);
