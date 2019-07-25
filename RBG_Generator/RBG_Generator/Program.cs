ade0f51bdbf4c0d1acedd274c2df1ff4bc425ae9
FunbiOyede <oyedetobi49@gmail.com>
2019-07-25 12:57:24 +0100

    rgb generator and final changes on the readline project

---
+++ RBG_Generator/RBG_Generator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RBG_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Random randomNumber = new Random();
+            var RedNumber = randomNumber.Next(256);
+            var BlueNumber = randomNumber.Next(256);
+            var GreenNumber = randomNumber.Next(256);
+
+            Console.WriteLine("color:  rgb({0},{1},{2})",RedNumber,BlueNumber,GreenNumber);
+        }
+
+
+        
+    }
+}
